1487b8097bcee06ef1fbc0f275153c09afc27002
Sara-bit-design <saradhingra3@gmail.com>
2021-12-01 10:40:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import react, { Component } from 'react';
-import './App.css';
-import Child from './components /child';
-
+import react, { Component } from "react";
+import "./components /child.css";
+import Child from "./components /child";
 
 class App extends Component {
-  state= {
-    title :'Parent Title'
-  }
-  changeTheTitle = (newTitle) =>{
-    this.setState({title:newTitle});
-  }
+  state = {
+    title: "Parent Title",
+  };
+  changeTheTitle = (newTitle) => {
+    this.setState({ title: newTitle });
+  };
   render() {
-     return (
-    <div className="App">
-      <Child ParentHandler ={this.changeTheTitle.bind(this,'Child Title')} title={this.state.title}/>
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Child
+          ParentHandler={this.changeTheTitle.bind(this, "Child Title")}
+          title={this.state.title}
+        />
+      </div>
+    );
+  }
 }
 export default App;
